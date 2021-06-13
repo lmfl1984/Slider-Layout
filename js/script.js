098f0202aea55b18c10d1f360b4719a7ddc5fd7b
Latkin Mikhail <82706373+lmfl1984@users.noreply.github.com>
2021-06-13 10:04:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+"use strict" //new function ON
+
+$(document).ready(function(){
+   $('.slaider').slick({
+      arrows: true,
+      adaptiveHeight: true,
+      speed: 800,
+      autoplay: true,
+      aytoplaySpeed: 1000,
+      
+   });
+})
